aaed67f824c2c25f8435298d2c694cb236822f14
agrawalni <nagrawal25492@gmail.com>
2018-07-24 15:45:57 +0530

    Angular datatable integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ContactsService} from './services/contacts.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,12 @@ import {ContactsService} from './services/contacts.service';
   styleUrls: ['./app.component.css'],
   providers: [ContactsService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy, OnInit {
 
   public contacts: any = [];
+  public dtOptions: DataTables.Settings = {};
+
+  public dtTrigger: Subject<any> = new Subject();
 
   constructor(private contactService: ContactsService) {}
 
@@ -17,6 +21,13 @@ export class AppComponent implements OnInit {
     this.contactService.getContacts().then( (resp) => {
       console.log(resp);
       this.contacts = resp.contacts;
+      this.dtTrigger.next();
     });
   }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
 }
